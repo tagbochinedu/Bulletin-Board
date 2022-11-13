6cd5426356471e48a09c8f81a118614149d8c961
Tagbo Chinedu <johntagbo2@gmail.com>
2022-11-13 06:12:01 +0100

    post slice created

---
+++ src/Features/Post/postSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
+  },
+  {
+    id: "2",
+    title: "Slices...",
+    content: "The more I say Slice, the more I wanat pizza",
+  },
+];
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+});
+
+export const {} = postSlice.actions
+export default postSlice.reducer
