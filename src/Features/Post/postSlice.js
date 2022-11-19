8352ca01ea92cf72523fb1b7888f6882cd7e33e5
Tagbo Chinedu <johntagbo2@gmail.com>
2022-11-19 10:54:21 +0100

    async thunk code revision

--- src/Features/Post/postSlice.js
+++ src/Features/Post/postSlice.js
@@ -1,22 +1,9 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: { thumbsUp: 0, wow: 0, heart: 0, rocket: 0, coffee: 0 },
-  },
-  {
-    id: "2",
-    title: "Slices...",
-    content: "The more I say Slice, the more I want pizza",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: { thumbsUp: 0, wow: 0, heart: 0, rocket: 0, coffee: 0 },
-  },
-];
+const initialState = {
+  posts: [], status: 'idle', error:null
+}
 //This is a method which accepts an initial state, an object of reducer functions, and a "slice name", and automatically generates action creators and action types that correspond to the reducers and state
 const postSlice = createSlice({
   name: "posts",
@@ -24,7 +11,7 @@ const postSlice = createSlice({
   reducers: {
     postAdd: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       //In the event that the action.payload has to be the mutated a bit, the prepare() callback is used to make additions or remove things from it.
       prepare(body) {
@@ -42,15 +29,15 @@ const postSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
-      if (existingPost) {cd 
+      const existingPost = state.posts.find((post) => post.id === postId);
+      if (existingPost) {
         existingPost.reactions[reaction]++;
       }
     },
   },
 });
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 //the createSlice creates an action creator function which returns an action. An action is an object which contains type and payload keys
 export const { postAdd, reactionAdded } = postSlice.actions;
 
